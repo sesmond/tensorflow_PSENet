305bd0febed58d62606d1885fa17fbd56fdd3f5a
minjianxu <minjianxu@creditease.cn>
2019-12-23 15:44:16 +0800

    update

--- bin/train.sh
+++ bin/train.sh
@@ -2,5 +2,5 @@
 # 准备训练数据
 
 python -m train --gpu_list=0 --input_size=512 --batch_size_per_gpu=8 --checkpoint_path=./model/ \
---training_data_path=/Users/minjianxu/Documents/icdar/ctw1500/train/text_image
+--training_data_path=./data/ctw1500/train/text_image
 #--training_data_path=./data/ocr/icdar2015/
