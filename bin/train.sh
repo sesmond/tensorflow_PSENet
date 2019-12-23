b73cbf13ba34674696b30454508b2d079799a920
minjianxu <minjianxu@creditease.cn>
2019-12-23 16:02:07 +0800

    modify path

--- bin/train.sh
+++ bin/train.sh
@@ -1,7 +1,7 @@
 # 训练数据
 # 准备训练数据
 
-python -m train --gpu_list=0 --input_size=512 --batch_size_per_gpu=8 --checkpoint_path=./model \
+python -m train --gpu_list=0 --input_size=512 --batch_size_per_gpu=8 --checkpoint_path=/app/models/pse/ctw1500 \
 --training_data_path=./data/ctw1500/train/text_image \
 --training_text_path=./data/ctw1500/train/text_label_curve \
 
