89c785125a9fa5246da5fe4b20737434c2a688de
minjianxu <minjianxu@creditease.cn>
2019-12-23 15:58:28 +0800

    modify path

--- bin/train.sh
+++ bin/train.sh
@@ -1,6 +1,8 @@
 # 训练数据
 # 准备训练数据
 
-python -m train --gpu_list=0 --input_size=512 --batch_size_per_gpu=8 --checkpoint_path=./model/ \
---training_data_path=./data/ctw1500/train/text_image
+python -m train --gpu_list=0 --input_size=512 --batch_size_per_gpu=8 --checkpoint_path=./model \
+--training_data_path=./data/ctw1500/train/text_image \
+--training_text_path=./data/ctw1500/train/text_label_curve \
+
 #--training_data_path=./data/ocr/icdar2015/
