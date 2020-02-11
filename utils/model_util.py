9fcca4a350b26ff857b5f65d188dd4e09511f726
minjianxu <minjianxu@creditease.cn>
2020-02-11 19:47:49 +0800

    bugfix

--- utils/model_util.py
+++ utils/model_util.py
@@ -24,8 +24,8 @@ def save_model(sess,global_step,save_mod_dir):
         if not tf.gfile.Exists(cur):
             save_mod_dir = cur
             break
-    # tf.get_variable_scope().reuse_variables()
-    tf.reset_default_graph()
+    tf.get_variable_scope().reuse_variables()
+    # tf.reset_default_graph()
     # 定义张量
     input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
     seg_maps_pred = model.model(input_images, is_training=False)
